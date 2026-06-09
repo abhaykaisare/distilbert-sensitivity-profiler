c6f18ba5fba7a126e93853e76bd3371047523571
abhaykaisare <kaisareab@gmail.com>
2026-06-09 17:22:59 +0530

    Remove 4 and 5 file

--- 4_model_info.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoModelForSequenceClassification
-model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
-
-# Loop through named modules
-for name, module in model.named_modules():
-    # We only care about the actual mathematical layers doing the matrix math
-    if "Linear" in str(type(module)):
-        print(f"Layer Found: {name} | Type: {type(module)}")
